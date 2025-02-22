69e2f808381942005bf54de794f4cb8f098e2640
Shiddhartha-Pal <pal35-1069@diu.edu.bd>
2025-02-22 23:23:40 +0600

    just made a basic stracture

---
+++ src/components/pages/resume/index.tsx
@@ -0,0 +1,7 @@
+export default function resume() {
+  return (
+    <div>
+      <h1>resume</h1>
+    </div>
+  );
+}
---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,7 @@
+export function Layout() {
+  return (
+    <div>
+      <h1>defaultLayout</h1>
+    </div>
+  );
+}
